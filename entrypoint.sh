f0cd98731ea3bb9f5995290262f6baa37fc85013
david holton <david.holton@va.gov>
2023-01-25 16:52:13 -0500

    TAS-2826: GH Runner

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,9 @@ set -e
 
 df -h 
 
-pause 30
+echo "Hey FOOBAR, $(hostname) $(date)" >> /tmp/reports/Foo.out
+
+sleep 30
 
 all_args=$@
 /usr/share/dependency-check/bin/dependency-check.sh  ${all_args} 
