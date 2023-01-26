73b792f45ac3432640573c35a166440b6ec548b6
david holton <david.holton@va.gov>
2023-01-26 08:19:55 -0500

    TAS-2826: GHRunner

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
 
+echo;echo ======================================; echo First Explore Period
+sleep 300
+echo;echo ======================================; echo I woke up...Running DepCheck
+echo;echo
+
 all_args=$@
 /usr/share/dependency-check/bin/dependency-check.sh  ${all_args}
 
-echo;echo ======================================; echo Now Explore
+echo;echo ======================================; echo Post Explore Period
 sleep 300
 echo;echo ======================================; echo I woke up...Bye
